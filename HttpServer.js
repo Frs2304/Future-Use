c27f39b351bc95ea56c715c0fe9bea095525cb0e
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-22 18:26:06 +0530

    Initial commit

---
+++ HttpServer.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req,red)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html')
+    res.end('<h1> Hi! This is Tushar </h1> <p> Hey! this is the way to rock the code </p>' );
+
+})
+server.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`);
+})
