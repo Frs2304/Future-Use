f293e41afce07bb37670e84ccf96a923d645d04e
Tushar Mishra <tusharmishra2304@gmail.com>
2023-03-16 23:58:49 +0530

    Update pathmodules.js

--- pathmodules.js
+++ pathmodules.js
@@ -2,6 +2,6 @@ const path = require('path');
 const a  = path.basename('C:\\temp\\myfile.html');
 const b  = path.dirname('C:\\temp\\myfile.html');
 const c = path.extname(__filename);
-console.log(a)
-console.log(b)
-console.log(c)
+console.log(a);
+console.log(b);
+console.log(c);
