75fb1329eed36dcddcc0fa6e415adf7907c490a4
Tushar Mishra <tusharmishra2304@gmail.com>
2023-03-17 00:00:17 +0530

    Update fsmodules.js

--- fsmodules.js
+++ fsmodules.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 fs.readFile('second.js','utf8',(err,data)=>{
-    console.log(err,data)
+    console.log(err,data);
 })
-console.log("Finished reading file") 
+console.log("Finished reading file");
