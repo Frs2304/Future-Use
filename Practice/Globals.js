db1cbe2ebbc02e525bd8d7b60aecbfbddf765d47
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-05-21 17:16:04 +0530

    Initial commit

---
+++ Practice/Globals.js
@@ -0,0 +1,7 @@
+// Globals in Node.js
+
+// __dirname - path to ciurrent directory
+// __filename - file name
+// require - functions to use modeules (Common JS)
+// module - info about current module(file)
+// process - info about env where the progress is being executed
