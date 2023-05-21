8996ed2ab8064598dce8486b7e028f33358ad953
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-05-22 01:35:00 +0530

    Updated FsModulesAsync.js

--- Practice/Built-in Modules/Fs Modules/FsModulesAsync.js
+++ Practice/Built-in Modules/Fs Modules/FsModulesAsync.js
@@ -1,5 +1,6 @@
  const {  readFile, writeFile } = require('fs');
 
+ // Asynchronously reads the entire contents of a file
  readFile('./Practice/Built-in Modules/Fs Modules/FsModulesSync.js', 'utf8', (err, result) => {
     if(err){
         console.log(err);
@@ -8,6 +9,7 @@
     console.log(result);
  });
 
+// Asynchronously reads the entire contents of a file
  writeFile('Practice/Built-in Modules/Fs Modules/second.txt',
  `Here is the result: ${first}, ${second}`, (err, result) => {
     if(err) {
