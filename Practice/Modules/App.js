6084a2b5ed7a76940a0886fa08fc080f581808ae
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-05-21 17:16:35 +0530

    Initial commit

---
+++ Practice/Modules/App.js
@@ -0,0 +1,10 @@
+// Importing Names.js
+const names = require('./Names');
+const sayHi = require('./Ulits');
+
+// Importing Utils.js
+const utils = require('./Ulits');
+
+console.log(names);
+// Calling function sayHi (imported form Utils.js)
+sayHi('Tushar');
