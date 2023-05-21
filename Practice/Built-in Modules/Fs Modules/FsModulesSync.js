f35d8d0eaaad1b8649c97ab5d55c8b4230c57769
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-05-21 23:58:34 +0530

    Updated FsModulesSync.js

--- Practice/Built-in Modules/Fs Modules/FsModulesSync.js
+++ Practice/Built-in Modules/Fs Modules/FsModulesSync.js
@@ -5,3 +5,8 @@ const first = readFileSync('first.txt','utf8');
 const second = readFileSync('.second.txt','utf8');
 
 console.log(first,second);
+
+writeFileSync(
+    'result.txt',
+    `Here is the result : ${first}, ${second}`
+)
