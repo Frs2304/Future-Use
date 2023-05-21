3fa4ddef454bc1d61a2d7d8b5f71f8d21809da1e
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-05-21 17:16:45 +0530

    Initial commit

---
+++ Practice/Modules/Names.js
@@ -0,0 +1,10 @@
+// Local variable
+const secret = "Super Secret";
+// Varibles that will be shared
+const tushar = "Tushar";
+const yash = "Yash";
+
+console.log(secret);
+ 
+// exporting
+module.exports = {tushar,yash}
