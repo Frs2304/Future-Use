8460614c32b45c7e78280bc5697c3078a9120f52
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-05-22 17:01:45 +0530

    Updated HttpModules.js

--- Practice/Built-in Modules/HttpModules.js
+++ Practice/Built-in Modules/HttpModules.js
@@ -2,6 +2,12 @@ const http = require('http');
 const port = 3000;
 // Returns a new instance of Server.
 const server = http.createServer((req,res) => {
-    res.end('Welcome to my homepage')
+    if(req.url === '/'){
+        res.end('Welcome to my Homepage')
+    }
+    if(req.url === '/about'){
+    res.end('Welcome to the About page')
+    }
 })
+// Start a server listening for connections.
 server.listen(port); 
