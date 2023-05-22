5fe19f772d12f58f5c0ba70ded54728939cbe367
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-05-22 17:15:53 +0530

    Updated Globals.js

--- Practice/Globals.js
+++ Practice/Globals.js
@@ -5,3 +5,18 @@
 // require - functions to use modeules (Common JS)
 // module - info about current module(file)
 // process - info about env where the progress is being executed
+
+// npm
+
+// npm - global command, comes with node
+// npm--version - to check the node version that is installled on your system
+
+// local dependency - use it only in this project
+// npm install - to install packages 
+
+// global dependency - use it in any project
+// npm install -g <packagename> - to install packages globally
+
+// package.json - manaifest file (contains information about project/dependencies)
+
+// npm init - to initialize a node project
