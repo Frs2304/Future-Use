86c68eb69563327a45675c04eccc927cafa483b0
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-06-02 23:58:08 +0530

    Updated Stream.js

--- Practice/Built-in Modules/Stream.js
+++ Practice/Built-in Modules/Stream.js
@@ -1,7 +1,8 @@
+// import createReadStream from 'fs' module
 const { createReadStream } = require('fs');
 
 const stream = createReadStream('./OsModules.js');
 
 stream.on('data', (result) => {
-    console.log(result)
+    console.log(result);
 })
